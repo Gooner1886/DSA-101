f4ce710f3cfec58245ede5b91f822799727df2ee
Siddharth Sarma <siddharthgsarma5@gmail.com>
2022-01-30 00:47:19 +0530

    added populating next right pointers II

---
+++ bfs/populatingNextRightPointers2.java
@@ -0,0 +1,26 @@
+// Leetcode - 117 - Populating Next Right Pointers in Each Node II
+public Node connect(Node root) {
+    if(root == null) {
+        return null;
+    }
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(root);
+    while(!queue.isEmpty()) {
+        int size = queue.size();
+        for(int i = 0; i < size; i++) {
+            Node current = queue.remove();
+            if(i == size - 1) {
+                current.next = null;
+            } else {
+                current.next = queue.peek();
+            }
+            if(current.left != null) {
+                queue.add(current.left);
+            }
+            if(current.right != null) {
+                queue.add(current.right);
+            }
+        }
+    }
+    return root;
+}
