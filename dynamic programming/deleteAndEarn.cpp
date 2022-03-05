00379294113b6c512c607a49ff7cdaaf4e188067
Siddharth Sarma <siddharthgsarma5@gmail.com>
2022-03-05 18:05:46 +0530

    updated delete and earn

--- dynamic programming/deleteAndEarn.cpp
+++ dynamic programming/deleteAndEarn.cpp
@@ -1,26 +1,46 @@
 // Leetcode - 740 - Delete and earn
+// int deleteAndEarn(vector<int> &nums)
+// {
+//     if (nums.size() == 0)
+//         return nums[0];
+
+//     vector<int> hash(10001, 0);
+//     int maxTmp = INT_MIN;
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         hash[nums[i]]++;
+//         if (nums[i] > maxTmp)
+//         {
+//             maxTmp = nums[i];
+//         }
+//     }
+//     int incl = 0;
+//     int excl = 0;
+//     for (int i = 0; i <= maxTmp; i++)
+//     {
+//         int temp = excl;
+//         excl = max(incl, excl);
+//         incl = temp + hash[i] * i;
+//     }
+//     return max(incl, excl);
+// }
 int deleteAndEarn(vector<int> &nums)
 {
-    if (nums.size() == 0)
-        return nums[0];
+    const int N = 10001;
+    vector<int> frequency(N, 0);
 
-    vector<int> hash(10001, 0);
-    int maxTmp = INT_MIN;
-    for (int i = 0; i < nums.size(); i++)
+    for (auto num : nums)
     {
-        hash[nums[i]]++;
-        if (nums[i] > maxTmp)
-        {
-            maxTmp = nums[i];
-        }
+        frequency[num]++;
     }
-    int incl = 0;
-    int excl = 0;
-    for (int i = 0; i <= maxTmp; i++)
+
+    int dp[N];
+    dp[0] = 0;
+    dp[1] = frequency[1];
+
+    for (int i = 2; i < N; i++)
     {
-        int temp = excl;
-        excl = max(incl, excl);
-        incl = temp + hash[i] * i;
+        dp[i] = max(dp[i - 2] + i * frequency[i], dp[i - 1]);
     }
-    return max(incl, excl);
+    return dp[N - 1];
 }
