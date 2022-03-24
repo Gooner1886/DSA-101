db2bb9506d424a4ce27c281354cc00f1914970fc
Siddharth Sarma <siddharthgsarma5@gmail.com>
2022-03-24 11:49:33 +0530

    add broken calculator

---
+++ maths/brokenCalculator.cpp
@@ -0,0 +1,24 @@
+// Leetcode - 991 - Broken Calculator
+int brokenCalc(int startValue, int target)
+{
+    long long ans = 0;
+    long long tar = target;
+    while (startValue != tar)
+    {
+        if (tar % 2 == 0 and startValue < tar)
+        {
+            tar /= 2;
+        }
+        else if (tar > startValue)
+        {
+            tar += 1;
+        }
+        else
+        {
+            ans += abs(tar - startValue) - 1;
+            tar = startValue;
+        }
+        ans++;
+    }
+    return ans;
+}
