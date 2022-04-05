3a598575936eecbc2b10fa309f675bba1d476e1b
Siddharth Sarma <siddharthgsarma5@gmail.com>
2022-04-05 08:34:53 +0530

    added interesection of two arrays

--- arrays/intersectionOfTwoArrays.cpp
+++ arrays/intersectionOfTwoArrays.cpp
@@ -1,17 +1,17 @@
-// Intersection of two arrays 2 - Leetcode
+// Leetcode - 349 - Intersection of Two Arrays
 
-vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> result;
-        unordered_map<int, int> um;
-        for(auto num: nums1) {
-            um[num]++;
-        }
-        for(auto num: nums2) {
-            if(um[num] > 0) {
-                result.push_back(num);
-                um[num]--;
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> unique;
+        vector<int> answer;
+        for(int i = 0; i < nums1.size(); i++) {
+            for(int j = 0; j < nums2.size(); j++) {
+                if(nums1[i] == nums2[j]) {
+                    unique.insert(nums1[i]);
+                }
             }
         }
-        
-        return result;
+        for(auto val: unique) {
+            answer.push_back(val);
+        }
+        return answer;
     }
