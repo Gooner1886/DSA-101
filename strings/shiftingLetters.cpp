b91d39a2e944200e185dfaff3ee2cc7589be293c
Siddharth Sarma <siddharthgsarma5@gmail.com>
2022-04-12 10:08:07 +0530

    added shiftingLetters

---
+++ strings/shiftingLetters.cpp
@@ -0,0 +1,11 @@
+// Leetcode - 848 - Shifting Letters
+string shiftingLetters(string s, vector<int> &shifts)
+{
+    int k = 0;
+    for (int i = s.size() - 1; i >= 0; i--)
+    {
+        k = (k + shifts[i]) % 26;
+        s[i] = (s[i] - 'a' + k) % 26 + 'a';
+    }
+    return s;
+}
